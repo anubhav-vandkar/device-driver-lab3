401353289cf582136c178c5b1cdd6dce6ff02f54
anubhav-vandkar <av3336@columbia.edu>
2026-03-26 14:34:56 -0400

    circle added input xy coords to centre

--- lab3-hw/vga_ball.sv
+++ lab3-hw/vga_ball.sv
@@ -66,10 +66,10 @@ module vga_ball(input logic        clk,
         3'h0 : background_r <= writedata;
         3'h1 : background_g <= writedata;
         3'h2 : background_b <= writedata;
-        // 3'h3 : begin
-        //   center_x <= writedata[31:22]; //x coordinate in upper 10 bits
-        //   center_y <= writedata[8:0];  //y coordinate in lower 9 bits
-        // end
+        3'h3 : begin
+          center_x <= writedata[31:22]; //x coordinate in upper 10 bits
+          center_y <= writedata[8:0];  //y coordinate in lower 9 bits
+        end
       endcase
 
   end
